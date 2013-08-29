4c49e4026b6f6efda28fb3bf06066649da52d96c
T-Okada55 <co.muska@gmail.com>
2013-08-30 01:29:35 +0900

    ツイッター投稿スクリプトを実用化
    接続モジュール変更
    投稿文変更

--- RaspberryPi_main/scripts/toTwitter.py
+++ RaspberryPi_main/scripts/toTwitter.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python
 import os.path
-import http.client
 
-argvs = sys.argv
-argc = len(argvs)
-if(argc != 2):
-	print 'Usage: # python toTwitter.py message'
-	quit()
-
-if(!os.path.exists("isBot"))
+if(not os.path.exists("isBot")):
 	print "User won't tweet"
 	quit()
 
-conn = htto.client.HTTPConnection('localhost/twitter.php?tweet=' + argv[1])
-conn.request("GET", "/")
-res = conn.getresponse()
-if(res.status == "200")
-	print "Success tweet: " + argv[1]
-else
-	print "Fail tweet: " + argv[1]
-conn.close()
+f = open('../var/www/data/csv', 'r')
+data = f.readlines()
+f.close()
+lastLine = data[-1]#インデックスが負だと後ろから数える
+temp = lastLine.split(',')
+
+temperature = temp[0]
+humidity = temp[1]
+message = "Smartfanが測定した温度は" + temperature +"度、湿度は" + humidity + "％です。"
+	
+from urllib import urlopen
+src = urlopen('http://localhost/tweet.php?tweet=' + message).read()
+print src 

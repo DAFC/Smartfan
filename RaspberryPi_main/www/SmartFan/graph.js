281f7b602e4800b58e2397af2303be7f97f76c4a
T-Okada55 <co.muska@gmail.com>
2013-08-30 01:32:29 +0900

    JavaScrptのバグフィックス+TypeScript掲載
    データの変化が少ないときにグラフの表示がおかしいバグを修正
    TypeScriptのコードも掲載

--- RaspberryPi_main/www/SmartFan/graph.js
+++ RaspberryPi_main/www/SmartFan/graph.js
@@ -1,21 +1,18 @@
-var DataName;
-(function (DataName) {
-    DataName[DataName["Temperature"] = 0] = "Temperature";
-    DataName[DataName["Humidity"] = 1] = "Humidity";
-})(DataName || (DataName = {}));
-
-var Graph = (function () {
-    function Graph(canvasId, dataName, dataURL) {
+﻿var Graph = (function () {
+    function Graph(canvasId, dataOffset, dataURL) {
         this.canvas = document.getElementById(canvasId);
         this.context = this.canvas.getContext('2d');
-        this.dataName = dataName;
+        this.dataOffset = dataOffset;
         this.dataURL = dataURL;
 
         this.context.fillStyle = "rgb(0,0,0)";
         this.context.strokeStyle = "rgb(32,32,32)";
+
+        this.offset = 20;
+        this.distance = 5;
     }
-    Graph.prototype.position = function (value, median, absmax) {
-        var v = (value - median) / absmax;
+    Graph.prototype.position = function (value) {
+        var v = (value - this.median) / this.absmax;
         v *= (this.canvas.height - 20) / 2;
         v += this.canvas.height / 2;
         return (this.canvas.height - v);
@@ -33,7 +30,7 @@ var Graph = (function () {
         delete req;
         var array = Array();
         data.forEach(function (line) {
-            var str = line.split(',')[_this.dataName];
+            var str = line.split(',')[_this.dataOffset];
             if ((str != null) && (str != "") && (str != undefined)) {
                 array.push(+str);
             }
@@ -52,47 +49,55 @@ var Graph = (function () {
             }
         });
 
+        this.median = median;
+        if (absmax > 0.1) {
+            this.absmax = absmax;
+        } else {
+            this.absmax = 0.1;
+        }
+        this.isDrawDeci = (this.absmax < 1);
+        this.isDrawOne = (this.absmax < 10);
+
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-        var near = Math.round(median / 10) * 10;
+        var nearTen = Math.round(median / 10 + 0.5) * 10;
+        var nearOne = Math.round(median + 0.5);
 
-        this.context.font = "12px 'sans-serif'";
-        this.context.strokeStyle = "rgb(128,128,128)";
-        for (var i = near - 50; i <= near + 50; i += 10) {
-            if (i != near) {
-                this.context.fillText(i.toString(), 0, this.position(i, median, absmax) - 5);
-                this.context.moveTo(0, this.position(i, median, absmax));
-                this.context.lineTo(this.canvas.width, this.position(i, median, absmax));
-            }
+        this.context.font = "20px 'sans-serif'";
+        this.context.strokeStyle = "rgb(0,0,0)";
+        for (var i = nearTen - 10; i <= nearTen + 10; i += 10) {
+            this.context.fillText(i.toString(), 0, this.position(i) + 6);
+            this.context.moveTo(this.offset, this.position(i));
+            this.context.lineTo(this.canvas.width, this.position(i));
         }
         this.context.stroke();
 
-        this.context.font = "20px 'serif'";
-        this.context.strokeStyle = "rgb(0,0,0)";
-        this.context.fillText(near.toString(), 0, this.position(near, median, absmax) - 5);
-        this.context.moveTo(0, this.position(near, median, absmax));
-        this.context.lineTo(this.canvas.width, this.position(near, median, absmax));
-        this.context.stroke();
+        if (this.isDrawOne) {
+            this.context.font = "12px 'sans-serif'";
+            this.context.strokeStyle = "rgb(32,32,32)";
+            for (var i = nearOne - 10; i <= nearOne + 10; i += 1) {
+                if (i % 10 != 0) {
+                    this.context.fillText(i.toString(), 0, this.position(i) + 4);
+                    this.context.moveTo(this.offset, this.position(i));
+                    this.context.lineTo(this.canvas.width, this.position(i));
+                }
+            }
+            this.context.stroke();
+        }
 
-        /*
-        this.context.fillText((near - 20).toString(), 0, this.position(near - 20, median, absmax) - 5);
-        this.context.moveTo(0, this.position(near - 20, median, absmax));
-        this.context.lineTo(this.canvas.width, this.position(near - 20, median, absmax));
-        this.context.fillText((near - 10).toString(), 0, this.position(near - 10, median, absmax) - 5);
-        this.context.moveTo(0, this.position(near - 10, median, absmax));
-        this.context.lineTo(this.canvas.width, this.position(near - 10, median, absmax));
-        this.context.fillText((near + 10).toString(), 0, this.position(near + 10, median, absmax) - 5);
-        this.context.moveTo(0, this.position(near + 10, median, absmax));
-        this.context.lineTo(this.canvas.width, this.position(near + 10, median, absmax));
-        this.context.fillText((near + 20).toString(), 0, this.position(near + 20, median, absmax) - 5);
-        this.context.moveTo(0, this.position(near + 20, median, absmax));
-        this.context.lineTo(this.canvas.width, this.position(near + 20, median, absmax));
-        this.context.stroke();
-        */
-        var distance = 10;
+        if (this.isDrawDeci) {
+            this.context.strokeStyle = "rgb(192,192,192)";
+            for (var i = nearOne - 10; i <= nearOne + 10; i += 0.1) {
+                if (nearOne != i && (i % 10 != 0)) {
+                    this.context.moveTo(this.offset, this.position(i));
+                    this.context.lineTo(this.canvas.width, this.position(i));
+                }
+            }
+            this.context.stroke();
+        }
 
         this.context.strokeStyle = "rgb(192,192,192)";
-        for (var i = 0; i <= this.canvas.width; i += distance * 2) {
+        for (var i = this.canvas.width; i > 0; i -= this.distance * 30) {
             this.context.moveTo(i, 0);
             this.context.lineTo(i, this.canvas.height);
         }
@@ -101,24 +106,17 @@ var Graph = (function () {
         this.context.strokeStyle = "rgb(32,32,32)";
         this.context.beginPath();
         for (var i = array.length; i >= 0; i--) {
-            this.context.lineTo(this.canvas.width - (i - 1) * distance, this.position(array[array.length - i], median, absmax));
+            this.context.lineTo(this.canvas.width - (i - 1) * this.distance, this.position(array[array.length - i]));
         }
         this.context.stroke();
-
-        for (var i = array.length; i >= 0; i--) {
-            this.context.beginPath();
-            this.context.arc(this.canvas.width - (i - 1) * distance, this.position(array[array.length - i], median, absmax), 2, 0, Math.PI * 2, false);
-            this.context.fill();
-        }
     };
 
-    Graph.prototype.start = function () {
+    Graph.prototype.Start = function () {
         var _this = this;
         this.Draw();
         this.timerToken = setInterval(function () {
             _this.Draw();
-        }, 5000);
+        }, 20000);
     };
     return Graph;
 })();
-//@ sourceMappingURL=app.js.map
---
+++ RaspberryPi_main/www/SmartFan/remote.js
@@ -0,0 +1,347 @@
+﻿function isNumeralCorrect(formName, elementName, minValue, maxValue) {
+    var str = document.forms[formName].elements[elementName].value;
+    if (!(/^-?\d+(\.\d+)?$/.test(str))) {
+        alert("数字を入力してください。");
+        return false;
+    }
+
+    var value = Number(str);
+    if (minValue > value) {
+        alert("値が小さすぎます");
+        return false;
+    }
+
+    if (maxValue !== "undefined" && maxValue < value) {
+        alert("値が大きすぎます。");
+        return false;
+    }
+
+    return true;
+}
+
+function program_OnKeyDownEvent(event) {
+    if (event.keyCode == 13) {
+        return false;
+    }
+
+    return true;
+}
+
+function actionList_OnChange() {
+    var actionList = document.forms["program"].elements["actionList"];
+    if (actionList.selectedIndex < 0) {
+        return;
+    }
+    if (actionList.selectedIndex == 0) {
+        ClearDynamicControl();
+        return;
+    }
+
+    eval((actionList[actionList.selectedIndex]).value + "Control()");
+}
+
+function OnProgramControl() {
+    ClearDynamicControl();
+}
+
+function OffProgramControl() {
+    ClearDynamicControl();
+}
+
+function PowerProgramControl() {
+    ClearDynamicControl();
+
+    var dc = document.getElementById("dynamicControl");
+
+    var lowRadio = document.createElement("input");
+    lowRadio.type = "radio";
+    lowRadio.name = "powerProgram";
+    lowRadio.value = "Low";
+    lowRadio.checked = true;
+    dc.appendChild(lowRadio);
+    dc.appendChild(document.createTextNode("弱"));
+
+    var middleRadio = document.createElement("input");
+    middleRadio.type = "radio";
+    middleRadio.name = "powerProgram";
+    middleRadio.value = "Middle";
+    dc.appendChild(middleRadio);
+    dc.appendChild(document.createTextNode("中"));
+
+    var highRadio = document.createElement("input");
+    highRadio.type = "radio";
+    highRadio.name = "powerProgram";
+    highRadio.value = "High";
+    dc.appendChild(highRadio);
+    dc.appendChild(document.createTextNode("強"));
+
+    var rhythmRadio = document.createElement("input");
+    rhythmRadio.type = "radio";
+    rhythmRadio.name = "powerProgram";
+    rhythmRadio.value = "Rhythm";
+    dc.appendChild(rhythmRadio);
+    dc.appendChild(document.createTextNode("リズム"));
+}
+
+function SwingProgramControl() {
+    ClearDynamicControl();
+
+    var dc = document.getElementById("dynamicControl");
+
+    var onRadio = document.createElement("input");
+    onRadio.type = "radio";
+    onRadio.name = "swingProgram";
+    onRadio.value = "On";
+    onRadio.checked = true;
+    dc.appendChild(onRadio);
+    dc.appendChild(document.createTextNode("On"));
+
+    var offRadio = document.createElement("input");
+    offRadio.type = "radio";
+    offRadio.name = "swingProgram";
+    offRadio.value = "Off";
+    dc.appendChild(offRadio);
+    dc.appendChild(document.createTextNode("Off"));
+}
+
+function ClearDynamicControl() {
+    var dc = document.getElementById("dynamicControl");
+    for (var i = dc.childNodes.length - 1; i >= 0; i--) {
+        dc.removeChild(dc.childNodes[i]);
+    }
+}
+
+function AddToDoList() {
+    if (!isNumeralCorrect("program", "minute", 0, "undefined")) {
+        return;
+    }
+
+    var actionList = document.forms["program"].elements["actionList"];
+
+    if (actionList.selectedIndex <= 0) {
+        return;
+    }
+
+    var selected = actionList[actionList.selectedIndex];
+    var minute = document.forms["program"].elements["minute"].value;
+    var toDoList = document.forms["program"].elements["toDoList[]"];
+
+    var text = "";
+    var keyValue = "";
+
+    if (selected.value == "OnProgram") {
+        text = selected.text + ":" + minute + "分後";
+        keyValue = minute + "_OnProgram";
+    } else if (selected.value == "OffProgram") {
+        text = selected.text + ":" + minute + "分後";
+        keyValue = minute + "_OffProgram";
+    } else if (selected.value == "PowerProgram") {
+        var radios = document.forms["program"].elements["powerProgram"];
+        var i = 0;
+        while (!(radios[i]).checked) {
+            i++;
+        }
+        text = selected.text + ":" + (radios[i].nextSibling).data + ":" + minute + "分後";
+        keyValue = minute + "_PowerProgram_" + (radios[i]).value;
+    } else if (selected.value == "SwingProgram") {
+        var radios = document.forms["program"].elements["swingProgram"];
+        var i = 0;
+        while (!(radios[i]).checked) {
+            i++;
+        }
+        text = selected.text + ":" + (radios[i].nextSibling).data + ":" + minute + "分後";
+        keyValue = minute + "_SwingProgram_" + (radios[i]).value;
+    }
+    toDoList.options[toDoList.length] = new Option(text, keyValue);
+
+    SortToDoList();
+}
+
+function SortToDoList() {
+    var toDoList = document.forms["program"].elements["toDoList[]"];
+    var tempArray = new Array();
+    var numberArray = new Array();
+
+    for (var i = 0; i < toDoList.length; i++) {
+        var toDo = toDoList[i];
+        var time = toDo.value.split("_")[0];
+        time = ("0000" + time).slice(-4);
+        if (typeof (numberArray[time]) === "undefined") {
+            numberArray[time] = 0;
+        }
+        var num = numberArray[time];
+        numberArray[time]++;
+        num = ("00" + num).slice(-2);
+        tempArray[i] = time + "\t" + num + "\t" + toDo.value + "\t" + toDo.text;
+    }
+
+    tempArray.sort();
+
+    var childArray = new Array();
+    for (var i = 0; i < tempArray.length; i++) {
+        childArray = tempArray[i].split("\t");
+        toDoList.options[i] = new Option(childArray[3], childArray[2]);
+    }
+}
+
+function DeleteToDoListItem() {
+    var toDoList = document.forms["program"].elements["toDoList[]"];
+    if (toDoList.selectedIndex < 0) {
+        return;
+    }
+
+    for (var i = toDoList.length - 1; i >= 0; i--) {
+        if ((toDoList.options[i]).selected) {
+            toDoList.options[i] = null;
+        }
+    }
+}
+
+function savedProgramList_OnChange() {
+    var spl = document.forms["program"].elements["savedProgramList"];
+    if (spl.selectedIndex < 0) {
+        return;
+    }
+    eval((spl[spl.selectedIndex]).value);
+}
+
+function savedProgramList_OnDoubleClick() {
+    (document.forms["program"].elements["savedProgramList"]).selectedIndex = -1;
+    (document.forms["program"].elements["saveProgramName"]).value = "";
+}
+
+function EditProgram(filename, program) {
+    ClearToDoListItems();
+
+    (document.forms["program"].elements["saveProgramName"]).value = filename;
+    var actions = program.split("|");
+    var toDoList = document.forms["program"].elements["toDoList[]"];
+
+    var text = null;
+    var keyValue = null;
+    var query = null;
+    for (var i = 0; i < actions.length; i++) {
+        text = "";
+        keyValue = "";
+        query = actions[i].split("_");
+        if (query[1] == "OnProgram") {
+            text = "起動:" + query[0] + "分後";
+        } else if (query[1] == "OffProgram") {
+            text = "終了:" + query[0] + "分後";
+        } else if (query[1] == "PowerProgram") {
+            text = "風量:" + ((query[2] == "low") ? "弱" : ((query[2] == "middle") ? "中" : ((query[2] == "high") ? "強" : (query[2] == "rhythm" ? "リズム" : "Unknown")))) + ":" + query[0] + "分後";
+        } else if (query[1] == "SwingProgram") {
+            text = "首振り:" + query[2] + ":" + query[0] + "分後";
+        }
+        keyValue = actions[i];
+        toDoList.options[toDoList.length] = new Option(text, keyValue);
+    }
+}
+
+function ClearToDoListItems() {
+    var toDoList = document.forms["program"].elements["toDoList[]"];
+
+    while (toDoList.length > 0) {
+        toDoList.options[toDoList.length - 1] = null;
+    }
+}
+
+function deleteProgramSubmit_OnClickEvent() {
+    if ((document.forms["program"].elements["savedProgramList"]).selectedIndex < 0) {
+        alert("削除するプログラムを選択してください。");
+        return false;
+    }
+    return true;
+}
+
+function saveProgramSubmit_ClickeEvent() {
+    if ((document.forms["program"].elements["saveProgramName"]).value == "") {
+        alert("プログラム名を入力してください。");
+        return false;
+    }
+
+    var toDoList = document.forms["program"].elements["toDoList[]"];
+
+    if (toDoList.childElementCount <= 0) {
+        alert('命令が空です。');
+        return false;
+    }
+
+    for (var i = toDoList.length - 1; i >= 0; i--) {
+        (toDoList.options[i]).selected = true;
+    }
+
+    return true;
+}
+
+function sendProgramSubmit_ClickeEvent() {
+    if ((document.forms["program"].elements["saveProgramName"]).value == "") {
+        alert("プログラム名を入力してください。");
+        return false;
+    }
+
+    var toDoList = document.forms["program"].elements["toDoList[]"];
+
+    if (toDoList.childElementCount <= 0) {
+        alert('命令が空です。');
+        return false;
+    }
+
+    for (var i = toDoList.length - 1; i >= 0; i--) {
+        (toDoList.options[i]).selected = true;
+    }
+
+    return true;
+}
+
+function Reloaded() {
+    ClearDynamicControl();
+
+    (document.getElementById("actionListDefault")).selected = true;
+    (document.forms["program"].elements["minute"]).value = "0";
+    (document.forms["program"].elements["savedProgramList"]).selectedIndex = -1;
+    (document.forms["program"].elements["saveProgramName"]).value = "";
+}
+
+var ValueGetter = (function () {
+    function ValueGetter(dataURL) {
+        this.dataURL = dataURL;
+    }
+    ValueGetter.prototype.Update = function () {
+        var req = new XMLHttpRequest();
+        req.open('GET', this.dataURL, false);
+        req.setRequestHeader('Pragma', 'no-cache');
+        req.setRequestHeader('Cache-Control', 'no-cache');
+        req.setRequestHeader('If-Modified-Since', 'Thu, 01 Jun 1970 00:00:00 GMT');
+        req.send();
+        var lines = req.responseText.split('\n');
+        delete req;
+
+        var lastLine = lines[lines.length - 1];
+        var data = lastLine.split(",");
+
+        if (data.length < 7) {
+            console.error("送られてくるデータが少なすぎます。");
+            return;
+        }
+
+        this.RewriteElements("switchInsert", (data[2] != "0") ? "On" : "Off");
+        this.RewriteElements("powerInsert", data[3] + "％");
+        this.RewriteElements("swingInsert", (data[4] != "0") ? "On" : "Off");
+        this.RewriteElements("temperatureSettingInsert", data[5] + "℃");
+    };
+
+    ValueGetter.prototype.RewriteElements = function (id, str) {
+        var elem = document.getElementById(id);
+        elem.textContent = str;
+    };
+
+    ValueGetter.prototype.Start = function () {
+        var _this = this;
+        this.Update();
+        this.timerToken = setInterval(function () {
+            _this.Update();
+        }, 20000);
+    };
+    return ValueGetter;
+})();
